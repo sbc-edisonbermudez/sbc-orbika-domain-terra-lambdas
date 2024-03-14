e34280fee8e6c82b4d8d2b3a970fa18071957a3f
Subocol <edisonbermudez@subocol.com>
2024-03-14 18:05:43 -0500

    Adjusting name

--- modules/sbc-module-aws-lambda/main.tf
+++ modules/sbc-module-aws-lambda/main.tf
@@ -51,12 +51,12 @@ resource "aws_iam_role" "lambda_role" {
 
 // Aprovisiona la lambda
 resource "aws_lambda_function" "lambda" {
-  function_name = "${var.function_name}-lambda"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.10"
-  filename      = "${path.module}/lambda_function.zip"
-  timeout       = var.timeout
+  function_name    = "${var.function_name}-lambda"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.10"
+  filename         = "${path.module}/lambda_function.zip"
+  timeout          = var.timeout
 
   vpc_config {
     subnet_ids         = [var.subnet_a_id, var.subnet_b_id]
