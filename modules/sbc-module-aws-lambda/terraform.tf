8e4e2a90337abcc8f3d59de24c9bde193c98f9e6
Subocol <edisonbermudez@subocol.com>
2024-03-19 16:20:18 -0500

    Ajusting providers

---
+++ modules/sbc-module-aws-lambda/terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.66.1"
+    }
+  }
+
+  backend "s3" {
+    bucket = "sbc-demo-tfstate-s3"
+    key    = "terraform/orbika/tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+provider "aws" {
+  region = local.aws_region
+}
+
