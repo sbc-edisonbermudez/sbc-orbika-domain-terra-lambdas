65fe09ff7e2e3e2eeef3f427ddc8a80fb5d58836
Subocol <edisonbermudez@subocol.com>
2024-03-19 17:32:41 -0500

    Ajusting providers

--- modules/sbc-module-aws-lambda/terraform.tf
+++ modules/sbc-module-aws-lambda/terraform.tf
@@ -3,7 +3,7 @@ terraform {
     aws = {
       source                = "hashicorp/aws"
       version               = "4.66.1"
-      configuration_aliases = [aws.foo]
+      configuration_aliases = [aws.foo, aws.s3]
     }
   }
 }
